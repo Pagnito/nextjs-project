2f903f290a32841faaceb4c60dc5938dd229e48c
pagnito <pagnito@gmail.com>
2021-09-25 13:47:11 -0400

    started

---
+++ components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import styles from './Nav.module.css';
+
+function Nav() {
+    return ( 
+        <div className={styles.navContainer}>
+            
+        </div>
+     );
+}
+
+export default Nav;
