233b984e82c1bd84854aa3537a48c6b6a814eb95
pagnito <pagnito@gmail.com>
2021-09-27 21:04:04 -0400

    finished animated css drawer menu pull out

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,15 +1,49 @@
 import styles from './Nav.module.css';
+import { createRef } from 'react/cjs/react.production.min';
 
 function Nav() {
+
   return (
     <div className={styles.navContainer}>
+
       <div className={styles.navPlaceholder}></div>
 
       <div className={styles.logo}>YouOnly</div>
-      <div className={styles.burgerMenu}>
+      <input id="checkBoxForButton" className={styles.checkBoxForButton} type="checkbox"></input>
+      <div className="drawerMenu"></div>
+      <label htmlFor="checkBoxForButton" className="burgerMenu">
         <div className={styles.burgerMenuBar}></div>
         <div className={styles.burgerMenuBar}></div>
-      </div>
+      </label>
+      <style jsx>{`
+      .burgerMenu {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        height: 20px;
+        right: 30px;
+        cursor: pointer;
+        position: absolute;
+        z-index: 10;
+      }
+      .drawerMenu {
+        height: 100vh;
+        width: 100%;
+        background-color: lightblue;
+        position: absolute;
+        top: 0;
+        transform: translateX(100%);
+        transition: all .3s ease-in-out;
+        z-index:9;
+      }
+        #checkBoxForButton:checked  + .drawerMenu {
+          transform: translateX(0%);
+        }
+        .burgerMenu:hover + .drawerMenu {
+          background-color: pink;
+        }
+      `}</style>
     </div>
   );
 }
